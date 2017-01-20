91fe2bacd3222b62c6e5048dd1ac803872b38871
Administrator <galibujianbusana@163.com>
2017-01-20 17:01:39 +0800

    <2017/1/20基本完成1>

--- app/src/main/java/myapplication/com/myplattvok/MainActivity.java
+++ app/src/main/java/myapplication/com/myplattvok/MainActivity.java
@@ -100,13 +100,13 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        if (id == R.id.action_seted) {
-
-
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        if (id == R.id.action_seted) {
+//
+//
+//        }
 
         return super.onOptionsItemSelected(item);
     }
